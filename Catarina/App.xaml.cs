e11f3fcbc3182bb510ce81c991d1147331eb3bcd
Золотов Ярослав Игоревич <ordsall@yandex.ru>
2018-04-19 14:38:06 +0300

    + Nlog Unhandled Ex

--- Catarina/App.xaml.cs
+++ Catarina/App.xaml.cs
@@ -13,5 +13,27 @@ namespace Catarina
     /// </summary>
     public partial class App : Application
     {
+
+        private static readonly NLog.ILogger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var config = new NLog.Config.LoggingConfiguration();
+            var fileTarget = new NLog.Targets.FileTarget() { FileName = "log.txt", Name = "logfile" };
+            config.AddTarget("logfile", fileTarget);
+            config.LoggingRules.Add(new NLog.Config.LoggingRule("*", NLog.LogLevel.Debug, fileTarget));
+            NLog.LogManager.Configuration = config;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Logger.Fatal(e.ExceptionObject as Exception, "Fatal UnhandledException");
+        }
     }
+
+      
 }
